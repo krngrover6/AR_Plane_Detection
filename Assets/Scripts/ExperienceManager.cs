70f5a0ab5d30f47e754bbe694a6d3ad00fbfa58b
Karan Grover <72027748+krngrover6@users.noreply.github.com>
2026-01-22 16:24:23 +0100

    Added the Experience Manager Script for position and rotation of the spawn Chicken

---
+++ Assets/Scripts/ExperienceManager.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class ExperienceManager : MonoBehaviour
+{
+    [SerializeField] private Button addChickenButton;
+    [SerializeField] private  ARRaycastManager arRaycastManager;
+    [SerializeField] private GameObject chickenPrefab;
+    
+    private bool _canAddChicken;
+    private GameObject _chickenPreview;
+    private Vector3 _detectedPosition;
+    private Quaternion _detectedRotation = Quaternion.identity;
+    private ARTrackable _currentTrackable;
+
+    private void Start()
+    {
+        InputHandler.OnTap += SpawnChicken;
+        _chickenPreview = Instantiate(chickenPrefab); //chicken preview
+        SetCanAddChicken(true);
+    }
+
+    private void SpawnChicken()
+    {
+        if(!_canAddChicken) return;
+        
+        var chicken = Instantiate(chickenPrefab); // Actual Spawned chicken
+        chicken.GetComponent<Chicken>().PlaceChicken(_currentTrackable);
+        chicken.transform.position = _detectedPosition;
+        chicken.transform.rotation = _detectedRotation;
+        
+        SetCanAddChicken(false); // User has to tap on the button to spawn new chicken
+    }
+
+    private void Update()
+    {
+        // Detect a position and rotation over the detected surface
+        GetRaycastHitTransform();
+    }
+
+    private void GetRaycastHitTransform()
+    {
+        var hits = new List<ARRaycastHit>();
+        var middleScreen = new Vector2(Screen.width / 2, Screen.height / 2);
+        if (arRaycastManager.Raycast(middleScreen, hits, TrackableType.PlaneWithinPolygon))
+        {
+            _detectedPosition = hits[0].pose.position;
+            _detectedRotation = hits[0].pose.rotation;
+            _chickenPreview.transform.position = _detectedPosition;
+            _chickenPreview.transform.rotation = _detectedRotation;
+            _currentTrackable = hits[0].trackable;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        InputHandler.OnTap -= SpawnChicken;
+    }
+
+
+    public void SetCanAddChicken(bool canAddChicken)
+    {
+        _canAddChicken = canAddChicken;
+        addChickenButton.gameObject.SetActive(_canAddChicken);
+        _chickenPreview.gameObject.SetActive(canAddChicken); 
+    }
+}
